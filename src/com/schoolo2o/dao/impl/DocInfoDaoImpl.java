c38bbe35a9a7e95baad105d6e4c29ba01f757004
herozhao <1014187702@qq.com>
2014-12-23 11:06:17 +0800

    zl

--- src/com/schoolo2o/dao/impl/DocInfoDaoImpl.java
+++ src/com/schoolo2o/dao/impl/DocInfoDaoImpl.java
@@ -1,35 +1,73 @@
 package com.schoolo2o.dao.impl;
 
 import java.math.BigInteger;
+import java.util.Iterator;
 import java.util.List;
+
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 
 import com.schoolo2o.dao.DocInfoDao;
 import com.schoolo2o.pojo.Docinfo;
+/*
+ * @author zhaolong
+ * 
+ */
 
-public class DocInfoDaoImpl implements DocInfoDao {
+//增加一个文档
+public class DocInfoDaoImpl extends HibernateDaoSupport implements DocInfoDao {
 
 	@Override
 	public boolean add(Docinfo doc) {
-		// TODO Auto-generated method stub
+		if(doc!=null){
+			try{
+				this.getHibernateTemplate().save(doc);
+				return true;
+			}catch(Exception e){
+				e.printStackTrace();
+				return false;
+			}
+		}
 		return false;
 	}
 
+	//删除一个文档
 	@Override
 	public boolean delete(BigInteger id) {
-		// TODO Auto-generated method stub
-		return false;
+		try{
+			String hql="from Docinfo where docId="+id;
+			List<Docinfo> list=this.getHibernateTemplate().find(hql);
+			Iterator it=list.iterator();
+			Docinfo di=(Docinfo) it.next();
+			this.getHibernateTemplate().delete(di);
+			return true;
+		}catch(Exception e){
+			e.printStackTrace();
+			return false;
+		}
 	}
 
+	//更新一份文档
 	@Override
 	public boolean update(Docinfo doc) {
-		// TODO Auto-generated method stub
-		return false;
+		try{
+			this.getHibernateTemplate().update(doc);
+			return true;
+		}catch(Exception e){
+			e.printStackTrace();
+			return false;
+		}
 	}
 
 	@Override
 	public List<Docinfo> search(Long userId) {
-		// TODO Auto-generated method stub
-		return null;
+		try{
+			String hql="from Docinfo where userId="+userId;
+			List<Docinfo> list=this.getHibernateTemplate().find(hql);
+			return list;
+		}catch(Exception e){
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 }
