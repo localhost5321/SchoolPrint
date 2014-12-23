677403a827c7f3873c361fd64425f98f0b7efd10
herozhao <1014187702@qq.com>
2014-12-23 10:55:29 +0800

    e

--- src/com/schoolo2o/dao/AdminInfoDao.java
+++ src/com/schoolo2o/dao/AdminInfoDao.java
@@ -10,9 +10,9 @@ import com.schoolo2o.pojo.Admininfo;
  */
 
 public interface AdminInfoDao {
-	//test
+	
 	public  boolean add(Admininfo admin);        /*增添管理员*/
 	public boolean update(Admininfo admin);      /*更新管理员信息*/
 	public boolean delete(String adminName);      /*根据管理员的登录名删除管理员*/
-	public List<Admininfo> search(Admininfo admin); /*查询是否有这个管理员*/
+	public List<Admininfo> search(String adminName); /*查询是否有这个管理员*/
 }
