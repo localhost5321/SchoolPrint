7c5fcd05d9a1d47d2b3d081017b2a5c5762bc3a6
lirongheng <361339766@qq.com>
2014-12-25 22:23:05 +0800

    12.25商家端首页更新

---
+++ WebRoot/js/index_shop.js
@@ -0,0 +1,30 @@
+/**
+ * 商家端主页js文件
+ */
+
+ 
+$(function() {
+	
+	$("#order_details").click(
+			
+	    function() {
+	    	$("#details_ul").css("display","block");
+		}
+			
+	);
+	
+	/*//显示订单详情
+	function showDetails() {
+		
+		alert("显示");
+		flag = true;
+	}
+	
+	//隐藏订单详情
+	function hideDetails(){
+		alert("隐藏");
+		flag = false;
+	}*/
+	
+});
+
