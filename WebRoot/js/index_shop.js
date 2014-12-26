b99b8a918686328c33e8665777a9fd24258f09ac
lirongheng <361339766@qq.com>
2014-12-26 13:51:53 +0800

    i

--- WebRoot/js/index_shop.js
+++ WebRoot/js/index_shop.js
@@ -1,30 +1,25 @@
 /**
  * 商家端主页js文件
  */
-
- 
 $(function() {
 	
 	$("#order_details").click(
 			
 	    function() {
-	    	$("#details_ul").css("display","block");
+	    	var value = $(this).text();
+	    	if(value == "订单详情"){
+	    		$("#details_ul").show();
+	    		//$("#details_ul").css("display","block");
+		    	$(this).text("收起>>");
+	    	}else{
+	    		$("#details_ul").hide();
+	    		//$("#details_ul").css("display","none");
+	    		$(this).text("订单详情");
+	    	}
+	    	
 		}
 			
 	);
-	
-	/*//显示订单详情
-	function showDetails() {
-		
-		alert("显示");
-		flag = true;
-	}
-	
-	//隐藏订单详情
-	function hideDetails(){
-		alert("隐藏");
-		flag = false;
-	}*/
 	
 });
 
