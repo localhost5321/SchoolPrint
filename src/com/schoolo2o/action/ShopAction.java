a822e25c075034c74956e05f0b11aec7e565387b
herozhao <1014187702@qq.com>
2014-12-30 21:06:44 +0800

    提交

--- src/com/schoolo2o/action/ShopAction.java
+++ src/com/schoolo2o/action/ShopAction.java
@@ -1,6 +1,7 @@
 package com.schoolo2o.action;
 
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -11,11 +12,16 @@ import org.apache.struts2.ServletActionContext;
 import com.alibaba.fastjson.JSON;
 import com.opensymphony.xwork2.ActionSupport;
 import com.schoolo2o.pojo.MyJSONObject;
+import com.schoolo2o.pojo.Orderinfo;
+import com.schoolo2o.pojo.Priceinfo;
+import com.schoolo2o.pojo.ShopComment;
 import com.schoolo2o.pojo.Shopinfo;
+import com.schoolo2o.service.OrderService;
 import com.schoolo2o.service.ShopService;
 
 public class ShopAction extends ActionSupport {
 	private ShopService shopService;
+	private OrderService orderService;
 	private MyJSONObject jsonObject=new MyJSONObject();
 	private HttpServletResponse response=ServletActionContext.getResponse();
 	private HttpServletRequest request = ServletActionContext.getRequest();
@@ -26,6 +32,16 @@ public class ShopAction extends ActionSupport {
 	public void setShopService(ShopService shopService) {
 		this.shopService = shopService;
 	}
+	
+	
+	public OrderService getOrderService() {
+		return orderService;
+	}
+
+	public void setOrderService(OrderService orderService) {
+		this.orderService = orderService;
+	}
+
 	/**
 	 * 获取商店列表
 	 * @throws IOException
@@ -33,22 +49,23 @@ public class ShopAction extends ActionSupport {
 	public  String getAllShops() throws IOException{
 		List<Shopinfo> list=shopService.searchShop();
 		response.setContentType("text/plain");
-		response.setCharacterEncoding("utf-8");
+		request.setCharacterEncoding("utf-8");
 		if(list!=null&&!list.isEmpty()){
+			String str=list.iterator().next().getShopDesc();
+			System.out.println(str);
 			jsonObject.setStatus("1");
 			jsonObject.setMessage("null");
 			jsonObject.setData(list);
 			String jsonStr=JSON.toJSONString(jsonObject);
-			System.out.println("fhdsughfu");
 			System.out.println(jsonStr);
-			response.getWriter().write(jsonStr);// 此处没有写完，到时候用json转化后，返回json字符串
+			response.getWriter().write(jsonStr);
 			return null;
 		}else{
 			jsonObject.setStatus("0");
 			jsonObject.setMessage(" 请求错误");
 			jsonObject.setData(null);
 			String jsonStr=JSON.toJSONString(jsonObject);
-			response.getWriter().write(jsonStr);//
+			response.getWriter().write(jsonStr);
 			return null;
 		}
 	}
@@ -63,9 +80,26 @@ public class ShopAction extends ActionSupport {
 		String shopName=request.getParameter("userName");
 		Shopinfo shop=shopService.search(shopName);
 		if(shop!=null){
-			response.getWriter().write(shop.toString());
+			List<ShopComment> comment=shopService.getComments(shopName);
+//			List <Priceinfo> priceType=shopService.getTypePrice(shopName);
+			List<Orderinfo> orders=orderService.shopSearch(shopName);
+//			shop.setOrderinfos(new HashSet(orders));
+//			shop.setPriceinfos(new HashSet(priceType));
+			shop.setShopComments(new HashSet(comment));
+//			System.out.println(shop.getShopName());
+			jsonObject.setStatus("1");
+			jsonObject.setMessage("null");
+			jsonObject.setData(shop);
+			String jsonStr=JSON.toJSONString(jsonObject);
+			response.getWriter().write(jsonStr);//
+			
 		}else{
-			response.getWriter().write("null");//此处待完善
+			jsonObject.setStatus("0");
+			jsonObject.setMessage(" 请求错误");
+			jsonObject.setData(null);
+			String jsonStr=JSON.toJSONString(jsonObject);
+			response.getWriter().write(jsonStr);//
+			
 		}
 		return null;
 	}
