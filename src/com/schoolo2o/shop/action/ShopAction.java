9f1926c16a78c79169d8c51c773c114a35e81a65
sunhengzhe <825980764@qq.com>
2015-01-15 16:15:27 +0800

    adgashfsjhfksjdhgkjsdhgsnbdfjhgkjsdgmjdkhkjfdhvbjfdbjkfdhvbksgjdghskjldahgkjsdhg

--- src/com/schoolo2o/shop/action/ShopAction.java
+++ src/com/schoolo2o/shop/action/ShopAction.java
@@ -19,6 +19,13 @@ import com.schoolo2o.service.ShopService;
  */
 public class ShopAction extends ActionSupport{
 	private ShopService shopService ;
+	public ShopService getShopService() {
+		return shopService;
+	}
+	public void setShopService(ShopService shopService) {
+		this.shopService = shopService;
+	}
+
 	private Shopinfo shopInfo;
 	private Map<String, Object> session = ServletActionContext.getContext().getSession();
 	private HttpServletResponse response = ServletActionContext.getResponse();
@@ -34,7 +41,14 @@ public class ShopAction extends ActionSupport{
 	public String shopLogin() throws IOException{
 		response.setContentType("text/plain");
 		response.setCharacterEncoding("utf8");
+		System.out.println("shopInfo = "+shopInfo);
+		System.out.println("shopInfo.shopName = "+shopInfo.getShopName());
+		System.out.println("shopPwd = "+shopInfo.getShopPwd());
+		System.out.println("this = "+this);
+		System.out.println("this.shopService = "+this.shopService);//null
+		System.out.println("shop = "+this.shopService.search(shopInfo.getShopName()));
 		Shopinfo shop = this.shopService.search(shopInfo.getShopName());
+		
 		if(shop == null){
 			response.getWriter().write("{\"status\":\"0\",\"message\":\"商店不存在\"}");
 			return null;
