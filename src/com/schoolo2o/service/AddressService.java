a9531d36ff71eb94b72b2d0435f75e55b37d3c4d
Administrator <Administrator@PC201306221225>
2015-01-16 21:45:18 +0800

    复合肥

---
+++ src/com/schoolo2o/service/AddressService.java
@@ -0,0 +1,30 @@
+package com.schoolo2o.service;
+
+import java.util.List;
+
+import com.schoolo2o.pojo.Addressinfo;
+
+public interface AddressService {
+	/**
+	 * 增加或者修改一条地址信息
+	 * @return
+	 */
+	public boolean addOrUpdateAddress(Addressinfo address );
+	/**
+	 * 删除一个地址信息
+	 * @param address
+	 * @return
+	 */
+	public boolean deleteAddress(long id);
+	
+	/**
+	 * 获得一个地址列表
+	 * @param userId
+	 * @return
+	 */
+	public List<Addressinfo> getAddresses(long userId);
+	
+	
+	
+
+}
