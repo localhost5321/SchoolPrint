0e814e8bc35f53dcf328ec692870143095acdf85
恒毅 <@lisa_pc>
2015-03-03 19:29:37 +0800

    小改动

--- src/com/schoolo2o/action/ShopAction.java
+++ src/com/schoolo2o/action/ShopAction.java
@@ -2,7 +2,6 @@ package com.schoolo2o.action;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 
@@ -13,8 +12,8 @@ import org.apache.struts2.ServletActionContext;
 
 import com.alibaba.fastjson.JSON;
 import com.opensymphony.xwork2.ActionSupport;
-import com.schoolo2o.pojo.MyJSONObject;
 import com.schoolo2o.pojo.Orderinfo;
+import com.schoolo2o.pojo.PriceKV;
 import com.schoolo2o.pojo.Priceinfo;
 import com.schoolo2o.pojo.ShopComment;
 import com.schoolo2o.pojo.Shopinfo;
@@ -26,12 +25,47 @@ import com.schoolo2o.service.OrderService;
 import com.schoolo2o.service.ShopService;
 import com.schoolo2o.utils.Console;
 import com.schoolo2o.utils.ListChange;
+import com.schoolo2o.utils.MD5;
 import com.schoolo2o.utils.Sender;
 
-public class ShopAction extends BaseAction {
-	private ShopService shopService;
+/**
+ * 商店端的操作
+ * @author hua
+ *
+ */
+public class ShopAction extends ActionSupport{
+	
+	private ShopService shopService ;
 	private OrderService orderService;
+	private Shopinfo shopInfo;
+	private Map<String, Object> session = ServletActionContext.getContext().getSession();
+	private HttpServletResponse response = ServletActionContext.getResponse();
+	private HttpServletRequest request = ServletActionContext.getRequest();
+	
+	public ShopService getShopService() {
+		return shopService;
+	}
+	
+	public void setShopService(ShopService shopService) {
+		this.shopService = shopService;
+	}
+	
+	public OrderService getOrderService() {
+		return orderService;
+	}
 
+	public void setOrderService(OrderService orderService) {
+		this.orderService = orderService;
+	}
+	
+	public Shopinfo getShopInfo() {
+		return shopInfo;
+	}
+	
+	public void setShopInfo(Shopinfo shopInfo) {
+		this.shopInfo = shopInfo;
+	}
+	
 	/**
 	 * 获取商店列表
 	 * 
@@ -57,7 +91,8 @@ public class ShopAction extends BaseAction {
 			return null;
 		}
 	}
-
+	
+	
 	/**
 	 * 由商店名获取商店详情
 	 * 
@@ -102,20 +137,67 @@ public class ShopAction extends BaseAction {
 		}
 
 	}
-
-	public ShopService getShopService() {
-		return shopService;
+	
+	/**
+	 * 店家登录
+	 * @return
+	 * @throws IOException
+	 */
+	public String shopLogin() throws IOException{
+		response.setContentType("text/plain");
+		response.setCharacterEncoding("utf8");
+		System.out.println("shopInfo = "+shopInfo);
+		System.out.println("shopInfo.shopName = "+shopInfo.getShopName());
+		System.out.println("shopPwd = "+shopInfo.getShopPwd());
+		System.out.println("this = "+this);
+		System.out.println("this.shopService = "+this.shopService);//null
+		System.out.println("shop = "+this.shopService.search(shopInfo.getShopName()));
+		Shopinfo shop = this.shopService.search(shopInfo.getShopName());
+		Console.LOG(getClass(), shop.getShopPwd());
+		
+		if(shop == null){
+			response.getWriter().write("{\"status\":\"0\",\"message\":\"商店不存在\"}");
+			return null;
+		}
+		else if(shop.getShopPwd().equals(shopInfo.getShopPwd())){
+			session.put("shopInfo", shop);
+			response.getWriter().write("{\"status\":\"1\",\"message\":\"\",\"shop\":"+ shop +"}");
+			return null;
+		}
+		else{
+			response.getWriter().write("{\"status\":\"0\",\"message\":\"密码错误\"}");
+			return null;
+		}
 	}
-
-	public void setShopService(ShopService shopService) {
-		this.shopService = shopService;
-	}
-
-	public OrderService getOrderService() {
-		return orderService;
-	}
-
-	public void setOrderService(OrderService orderService) {
-		this.orderService = orderService;
+	
+	
+	/**
+	 * 店家注册
+	 * @return
+	 * @throws IOException 
+	 */
+	public String shopRegist() throws IOException{
+		response.setContentType("text/plain");
+		response.setCharacterEncoding("utf-8");
+		shopInfo.setShopPwd(MD5.md5(shopInfo.getShopPwd().getBytes()));
+		String priceKVs = request.getParameter("priceKV");
+		boolean addSucc = shopService.add(shopInfo);
+		
+		if(addSucc){
+			//反序列化价格列表的JSON，依次重组并添加到对应店铺
+			if(!priceKVs .equals("null")){
+				ArrayList<PriceKV> priceList = (ArrayList<PriceKV>) JSON.parseArray(priceKVs, PriceKV.class);
+				for(PriceKV p : priceList){
+					Priceinfo pi = new Priceinfo(shopService.search(shopInfo.getShopName()),
+							p.getPriceK(), Double.parseDouble(p.getPriceV()));
+					shopService.addTypePrice(pi, shopInfo.getShopName());
+				}
+			}
+			
+			response.getWriter().write("{\"status\":\"1\",\"message\":\"\"}");
+			return null;
+		}
+		response.getWriter().write("{\"status\":\"0\",\"message\":\"注册失败\"}");
+		return null;
 	}
 }
